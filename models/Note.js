cc0e86033748bb83a847ac773093bd52593b546d
mahsavahidinasab <mahsa.vahidinasab@gmail.com>
2026-09-03 18:23:09 -0400

    defining models

---
+++ models/Note.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+    trim: true,
+    maxlength: [100, 'Title cannot exceed 100 characters']
+  },
+  content: {
+    type: String,
+    required: [true, 'Content is required'],
+    trim: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Note', noteSchema);
