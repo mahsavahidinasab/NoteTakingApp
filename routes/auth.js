b6acd211656cff910b2edc6ecc71a586306ff06d
mahsavahidinasab <mahsa.vahidinasab@gmail.com>
2026-09-03 23:41:39 -0400

    adding the GoogleAuth20

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+router.get('auth/google/callback', passport.authenticate('google', {failureRedirect: '/'}),
+(req, res, next) =>{
+    if(error) {
+        return next(error);
+    } res.redirect('/');
+} 
+);
+
+router.get('/logout', (req, res) => {
+    req.logout(() => res.redirect('/'))
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,40 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 app.set('view engine', 'ejs');
+mongoose.connect(process.env.MONGO_URI);
+
 
 app.use(express.json());
 app.use(express.static('public'));
 
-require('dotenv').config();
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
-mongoose.connect(process.env.MONGO_URI);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refresgToken, profile, done) => {
+    return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => done(null, user));
+
+passport.deserializeUser((user, done) => done(null, user));
+
 
 
 
