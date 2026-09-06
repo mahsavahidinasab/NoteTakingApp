128f60bcb07c4fdb6c8d7383a9db36188ae9f31a
mahsavahidinasab <mahsa.vahidinasab@gmail.com>
2026-09-06 12:50:44 -0400

    fixing the redirect route issue on render

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const methodOverride = require('method-override');
 const configurePassport = require('./config/passport');
 
 const app = express();
+
 const port = process.env.PORT || 3000;
+app.set('trust proxy', 1);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
